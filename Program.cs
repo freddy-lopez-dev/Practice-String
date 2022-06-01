95f6b98511d0cc05afc08709051e388cec8ec974
Frederick Lopez <mtester010@gmail.com>
2022-06-01 13:45:09 -0500

    Defining important string methods

--- Program.cs
+++ Program.cs
@@ -1,2 +1,42 @@
-﻿string myString = "This is the string";
-string interpolatedString = $"This string is interpolated to say the number {5 + 5}";
+﻿//string myString = "This is the string";
+//string interpolatedString = $"This string is interpolated to say the number {5 + 5}"; // Immutable is replaceable with new values
+//myString = interpolatedString; // this will copy the value in interpolated string and replace the original value
+//interpolatedString = "New String"; //Creates new address/value and refers to it
+
+//Console.WriteLine(interpolatedString);
+//Console.WriteLine(myString);
+
+
+////mutable
+//int numberOne = 5;// address 1
+//int numberTwo = 6;// address 2
+
+//numberOne = numberTwo; // Change what is inside the address 2 to be the same as what's in the address 1
+
+
+string s1 = "My name is Fred";
+string s2 = "Fred";
+
+bool compareString = s1.Contains(s2);
+
+Console.WriteLine(compareString);
+
+//String Method
+//1. Replace - Replace a matching character in a string (str.Replace('what to replace', 'which to replace'))
+//2. Trim - Trimming unecessary values in a string
+//3. Substring - targeting the index of a string name.Substring(index, No.ofChar).
+//4.Split - splitting the spaces of a string and writeline
+//5. Contains - check if string is in a string
+//6. Stringbuilder
+//using System.Text;
+//StringBuilder builder = new StringBuilder();
+//List<int> ints = new List<int>();
+
+
+//sentinel value
+//-1 IndexOf
+
+/* Create a method for Capitalizing the first letter of a sentence
+ * Create a method for capitalizing the first letter of every word in the String.
+ * Create a method for reversing a string ("hello world" -> "dlrow olleh") using iteration (StringBuilder)
+ */
